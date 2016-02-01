f3fca304bc1f5218d504efc90c411a125ce7b93d
ZaydH <zayd.hammoudeh@sjsu.edu>
2016-01-31 23:08:05 -0800

    bigEq seems to be mostly working.  bigSubtract is working minus shaving off preceding zeros. bigAdd is working.

--- Homeworks/HW1/HW1_Trial/src/Main.hs
+++ Homeworks/HW1/HW1_Trial/src/Main.hs
@@ -1,11 +1,27 @@
 module Main where
 
+bigEq :: [Int] -> [Int] -> Bool
+bigEq a b
+    | lenA /= lenB = False
+    | False `elem` comparedList = False
+    | otherwise = True
+    where   lenA = length a
+            lenB = length b
+            comparedList = zipWith ((==)) a b 
+
 bigAdd :: [Int] -> [Int] -> [Int]
 bigAdd a b = bigAddSum
     where   (paddedA, paddedB) = padLists a b -- Pad the two arrays so they are the same length
             -- Zip the two lists
             zippedList = zipWith (+) paddedA paddedB
             bigAddSum = normalizeList zippedList
+
+bigSubtract :: [Int] -> [Int] -> [Int]
+bigSubtract a b = bigSubtractDiff
+    where   (paddedA, paddedB) = padLists a b -- Pad the two arrays so they are the same length
+            -- Zip the two lists
+            zippedList = zipWith (-) paddedA paddedB
+            bigSubtractDiff = normalizeList zippedList
             
 
 padLists :: [Int] -> [Int] -> ([Int], [Int])
@@ -19,14 +35,16 @@ padLists a b = (paddedA, paddedB)
 normalizeList :: [Int] -> [Int]
 normalizeList a = normalizedA
             -- Used to make sure each element is between 0 and 999
-    where   subtractionList = [ subtract | block <- a, let subtract = -1000 * (quot block 1000)]
+    where   subtractionList = [ subtract | block <- a, let subtract = (-maxBlockValue) * div block maxBlockValue]
             -- Carry over as needed 
-            carryList = [0] ++  [ carry | block <- a, let carry = quot block 1000]
+            carryList = 0 : [carry | block <- a, let carry = div block maxBlockValue]
             lastCarry = last carryList
             -- Combine original list plus sums.
             normalizedA = zipWith3 (\x y z -> x + y + z) a subtractionList carryList ++ (lastCarry > 0) ? ([lastCarry], [])
-                
 
+
+
+-- Used as a global to represent the maximum block size
 maxBlockValue :: Int
 maxBlockValue = 1000
 

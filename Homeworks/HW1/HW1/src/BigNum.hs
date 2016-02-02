95af896509cd983f04189eee1c66be35e4acdafc
ZaydH <zayd.hammoudeh@sjsu.edu>
2016-02-02 00:56:48 -0800

    Porting code from my trial project to the main BigNum project.  bigAdd working.  bigSubtract is working with some issues.

--- Homeworks/HW1/HW1/src/BigNum.hs
+++ Homeworks/HW1/HW1/src/BigNum.hs
@@ -16,6 +16,7 @@ module BigNum (
   bigPowerOf,
   prettyPrint,
   stringToBigNum,
+  padLists,
 ) where
 
 type Block = Int -- An Int from 0-999
@@ -28,7 +29,11 @@ bigAdd :: BigNum -> BigNum -> BigNum
 bigAdd x y = bigAdd' x y 0
 
 bigAdd' :: BigNum -> BigNum -> Block -> BigNum
-bigAdd' _ _ _ = error "Your code here"
+bigAdd' a b c = bigAddSum
+    where   (paddedA, paddedB) = padLists a b -- Pad the two arrays so they are the same length
+            -- Zip the two lists
+            zippedList = zipWith (+) paddedA paddedB
+            bigAddSum = normalizeList zippedList
 
 bigSubtract :: BigNum -> BigNum -> BigNum
 bigSubtract x y =
@@ -44,7 +49,14 @@ stripLeadingZeroes xs = xs
 
 -- Negative numbers are not supported, so you may throw an error in these cases
 bigSubtract' :: BigNum -> BigNum -> Block -> BigNum
-bigSubtract' _ _ _ = error "Your code here"
+bigSubtract' a b c = bigSubtractDiff
+    where   (paddedA, paddedB) = padLists a b -- Pad the two arrays so they are the same length
+            -- Zip the two lists
+            zippedList = zipWith (-) paddedA paddedB
+            -- After normalizing the list, shave off any trailing zeros so list is minimum length.
+            subtractDiff = (reverse . dropWhile (==0) . reverse . normalizeList) zippedList
+            -- If the list is empty, append one 0. 
+            bigSubtractDiff = subtractDiff ++ (null subtractDiff) ? ([], [0])
 
 bigEq :: BigNum -> BigNum -> Bool
 bigEq _ _ = error "Your code here"
@@ -83,3 +95,29 @@ stringToBigNum' (a:b:c:rest) = block : stringToBigNum' rest
   where block = read $ c:b:a:[]
 
 
+-- Given two lists of Blocks (in essentially Little Endian format),
+-- pad the shorter list with 0 value blocks and return two new lists. 
+padLists :: [Block] -> [Block] -> ([Block], [Block])
+padLists a b = (paddedA, paddedB)
+    where   lenA = length a
+            lenB = length b
+            maxLen = max lenA lenB
+            paddedA = a ++ replicate (max (maxLen - lenA) 0) 0
+            paddedB = b ++ replicate (max (maxLen - lenB) 0) 0
+
+-- Having fun making a ternary operator.
+-- Source for Original Idea: https://caffeinatedcode.wordpress.com/2008/04/11/haskell-the-ternary-operator/
+(?) :: Bool -> (t, t) -> t
+a ? (b, c) = if a then b else c
+
+
+-- Perform any necessary carries and ensure each block is between 0 and maxblock - 1
+normalizeList :: [Int] -> [Int]
+normalizeList a = normalizedA
+            -- Used to make sure each element is between 0 and maxblock - 1
+    where   subtractionList = [ subtract | block <- a, let subtract = (-maxblock) * div block maxblock]
+            -- Carry over as needed 
+            carryList = 0 : [carry | block <- a, let carry = div block maxblock]
+            lastCarry = last carryList
+            -- Combine original list plus sums.
+            normalizedA = zipWith3 (\x y z -> x + y + z) a subtractionList carryList ++ (lastCarry > 0) ? ([lastCarry], []) 

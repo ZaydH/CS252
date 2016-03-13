ef606efa73041752ef7f50943b8aec0bc6b55a9c
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2016-03-13 01:40:51 -0800

    More lab practice for midterm. Working on the exam study guide.

---
+++ Exam_Study_Guides/Midterm/Practice_Code/Lab08/jsonParser.hs
@@ -0,0 +1,99 @@
+import Text.ParserCombinators.Parsec
+import System.Environment
+
+data JValue = JString String
+            | JNumber Double
+            | JBool Bool
+            | JNull
+            | JObject [(String, JValue)]
+            | JArray [JValue]
+  deriving (Eq, Ord, Show)
+
+
+jsonFile :: GenParser Char st JValue
+jsonFile = do
+  result <- jsonElem
+  spaces
+  eof
+  return result
+
+jsonElem :: GenParser Char st JValue
+jsonElem = do
+  spaces
+  result <- jsonElem'
+  spaces
+  return result
+
+jsonElem' = jsonArr
+        <|> jsonString
+        <|> jsonBool
+        <|> jsonNull
+        <|> jsonNumber
+        <|> jsonObject
+--        <?> "json element"
+
+jsonString :: GenParser Char st JValue
+jsonString = jsonStringDQ <|> jsonStringSQ
+
+jsonNumber = do
+      spaces
+      numb <- many1 digit
+      spaces
+      return $ JString numb
+
+jsonStringDQ = do
+  char '"'
+  s <- many $ noneOf "\"" -- crude.  does not allow double quotes within strings
+  char '"'
+  return $ JString s
+
+jsonStringSQ = do
+  char '\''
+  s <- many $ noneOf "'" -- crude, same as above
+  char '\''
+  return $ JString s
+
+jsonBool = do
+  bStr <- string "true" <|> string "false"
+  return $ case bStr of
+    "true" -> JBool True
+    "false" -> JBool False
+
+jsonNull = do
+  string "null"
+  return JNull
+
+jsonArr = do
+  char '['
+  arr <- jsonElem `sepBy` (char ',')
+  char ']'
+  return $ JArray arr
+
+jsonObject = do
+  char '{'
+  spaces
+  tuples <- jTuple `sepBy` string ","  
+  spaces
+  char '}'
+  return $ JObject tuples
+
+jTuple = do 
+         spaces
+         str <- many1 letter
+         spaces
+         string ":"
+         elem <- jsonElem
+         spaces
+         return $ (str, elem)
+
+parseJSON :: String -> Either ParseError JValue
+parseJSON input = parse jsonFile "(unknown)" input
+
+main = do
+  args <- getArgs
+  p <- parseFromFile jsonFile (head args)
+  case p of
+    Left err  -> print err
+    Right json -> print json
+
+

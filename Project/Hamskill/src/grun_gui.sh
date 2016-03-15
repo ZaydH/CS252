024ad2622eb578e81219e759e7931b02a86d622d
Zayd Hammoudeh <zayd.hammoudeh@sjsu.edu>
2016-03-15 02:17:59 -0700

    Remaking the entire hamskill project.

---
+++ Project/Hamskill/src/grun_gui.sh
@@ -0,0 +1,2 @@
+grun='java org.antlr.v4.gui.TestRig'
+$grun $1 $2 $3 -gui < $4
---
+++ Project/Hamskill/src/hamskill/antlr/HaskellListener.java
@@ -0,0 +1,480 @@
+// Generated from Haskell.g4 by ANTLR 4.5.2
+package hamskill.antlr;
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link HaskellParser}.
+ */
+public interface HaskellListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#program}.
+	 * @param ctx the parse tree
+	 */
+	void enterProgram(HaskellParser.ProgramContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#program}.
+	 * @param ctx the parse tree
+	 */
+	void exitProgram(HaskellParser.ProgramContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#codeBlock}.
+	 * @param ctx the parse tree
+	 */
+	void enterCodeBlock(HaskellParser.CodeBlockContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#codeBlock}.
+	 * @param ctx the parse tree
+	 */
+	void exitCodeBlock(HaskellParser.CodeBlockContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#headerComment}.
+	 * @param ctx the parse tree
+	 */
+	void enterHeaderComment(HaskellParser.HeaderCommentContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#headerComment}.
+	 * @param ctx the parse tree
+	 */
+	void exitHeaderComment(HaskellParser.HeaderCommentContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#lineComment}.
+	 * @param ctx the parse tree
+	 */
+	void enterLineComment(HaskellParser.LineCommentContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#lineComment}.
+	 * @param ctx the parse tree
+	 */
+	void exitLineComment(HaskellParser.LineCommentContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#generalComment}.
+	 * @param ctx the parse tree
+	 */
+	void enterGeneralComment(HaskellParser.GeneralCommentContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#generalComment}.
+	 * @param ctx the parse tree
+	 */
+	void exitGeneralComment(HaskellParser.GeneralCommentContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#commentWord}.
+	 * @param ctx the parse tree
+	 */
+	void enterCommentWord(HaskellParser.CommentWordContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#commentWord}.
+	 * @param ctx the parse tree
+	 */
+	void exitCommentWord(HaskellParser.CommentWordContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#moduleDefinition}.
+	 * @param ctx the parse tree
+	 */
+	void enterModuleDefinition(HaskellParser.ModuleDefinitionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#moduleDefinition}.
+	 * @param ctx the parse tree
+	 */
+	void exitModuleDefinition(HaskellParser.ModuleDefinitionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#moduleOpen}.
+	 * @param ctx the parse tree
+	 */
+	void enterModuleOpen(HaskellParser.ModuleOpenContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#moduleOpen}.
+	 * @param ctx the parse tree
+	 */
+	void exitModuleOpen(HaskellParser.ModuleOpenContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#moduleFunctionList}.
+	 * @param ctx the parse tree
+	 */
+	void enterModuleFunctionList(HaskellParser.ModuleFunctionListContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#moduleFunctionList}.
+	 * @param ctx the parse tree
+	 */
+	void exitModuleFunctionList(HaskellParser.ModuleFunctionListContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#moduleClose}.
+	 * @param ctx the parse tree
+	 */
+	void enterModuleClose(HaskellParser.ModuleCloseContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#moduleClose}.
+	 * @param ctx the parse tree
+	 */
+	void exitModuleClose(HaskellParser.ModuleCloseContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#moduleName}.
+	 * @param ctx the parse tree
+	 */
+	void enterModuleName(HaskellParser.ModuleNameContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#moduleName}.
+	 * @param ctx the parse tree
+	 */
+	void exitModuleName(HaskellParser.ModuleNameContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#moduleFunctionName}.
+	 * @param ctx the parse tree
+	 */
+	void enterModuleFunctionName(HaskellParser.ModuleFunctionNameContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#moduleFunctionName}.
+	 * @param ctx the parse tree
+	 */
+	void exitModuleFunctionName(HaskellParser.ModuleFunctionNameContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#func}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunc(HaskellParser.FuncContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#func}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunc(HaskellParser.FuncContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#mainFunction}.
+	 * @param ctx the parse tree
+	 */
+	void enterMainFunction(HaskellParser.MainFunctionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#mainFunction}.
+	 * @param ctx the parse tree
+	 */
+	void exitMainFunction(HaskellParser.MainFunctionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#mainPrototype}.
+	 * @param ctx the parse tree
+	 */
+	void enterMainPrototype(HaskellParser.MainPrototypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#mainPrototype}.
+	 * @param ctx the parse tree
+	 */
+	void exitMainPrototype(HaskellParser.MainPrototypeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#mainHeader}.
+	 * @param ctx the parse tree
+	 */
+	void enterMainHeader(HaskellParser.MainHeaderContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#mainHeader}.
+	 * @param ctx the parse tree
+	 */
+	void exitMainHeader(HaskellParser.MainHeaderContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#mainStatement}.
+	 * @param ctx the parse tree
+	 */
+	void enterMainStatement(HaskellParser.MainStatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#mainStatement}.
+	 * @param ctx the parse tree
+	 */
+	void exitMainStatement(HaskellParser.MainStatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#mainWords}.
+	 * @param ctx the parse tree
+	 */
+	void enterMainWords(HaskellParser.MainWordsContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#mainWords}.
+	 * @param ctx the parse tree
+	 */
+	void exitMainWords(HaskellParser.MainWordsContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#parenMainWord}.
+	 * @param ctx the parse tree
+	 */
+	void enterParenMainWord(HaskellParser.ParenMainWordContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#parenMainWord}.
+	 * @param ctx the parse tree
+	 */
+	void exitParenMainWord(HaskellParser.ParenMainWordContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#generalMainWord}.
+	 * @param ctx the parse tree
+	 */
+	void enterGeneralMainWord(HaskellParser.GeneralMainWordContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#generalMainWord}.
+	 * @param ctx the parse tree
+	 */
+	void exitGeneralMainWord(HaskellParser.GeneralMainWordContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#funcPrototype}.
+	 * @param ctx the parse tree
+	 */
+	void enterFuncPrototype(HaskellParser.FuncPrototypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#funcPrototype}.
+	 * @param ctx the parse tree
+	 */
+	void exitFuncPrototype(HaskellParser.FuncPrototypeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#functionName}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionName(HaskellParser.FunctionNameContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#functionName}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionName(HaskellParser.FunctionNameContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#typeSignature}.
+	 * @param ctx the parse tree
+	 */
+	void enterTypeSignature(HaskellParser.TypeSignatureContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#typeSignature}.
+	 * @param ctx the parse tree
+	 */
+	void exitTypeSignature(HaskellParser.TypeSignatureContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#inputType}.
+	 * @param ctx the parse tree
+	 */
+	void enterInputType(HaskellParser.InputTypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#inputType}.
+	 * @param ctx the parse tree
+	 */
+	void exitInputType(HaskellParser.InputTypeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#returnType}.
+	 * @param ctx the parse tree
+	 */
+	void enterReturnType(HaskellParser.ReturnTypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#returnType}.
+	 * @param ctx the parse tree
+	 */
+	void exitReturnType(HaskellParser.ReturnTypeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#type}.
+	 * @param ctx the parse tree
+	 */
+	void enterType(HaskellParser.TypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#type}.
+	 * @param ctx the parse tree
+	 */
+	void exitType(HaskellParser.TypeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#unitType}.
+	 * @param ctx the parse tree
+	 */
+	void enterUnitType(HaskellParser.UnitTypeContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#unitType}.
+	 * @param ctx the parse tree
+	 */
+	void exitUnitType(HaskellParser.UnitTypeContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#funcbody}.
+	 * @param ctx the parse tree
+	 */
+	void enterFuncbody(HaskellParser.FuncbodyContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#funcbody}.
+	 * @param ctx the parse tree
+	 */
+	void exitFuncbody(HaskellParser.FuncbodyContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#funcStatement}.
+	 * @param ctx the parse tree
+	 */
+	void enterFuncStatement(HaskellParser.FuncStatementContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#funcStatement}.
+	 * @param ctx the parse tree
+	 */
+	void exitFuncStatement(HaskellParser.FuncStatementContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#patternMatchingArguments}.
+	 * @param ctx the parse tree
+	 */
+	void enterPatternMatchingArguments(HaskellParser.PatternMatchingArgumentsContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#patternMatchingArguments}.
+	 * @param ctx the parse tree
+	 */
+	void exitPatternMatchingArguments(HaskellParser.PatternMatchingArgumentsContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#patternMatchingArgument}.
+	 * @param ctx the parse tree
+	 */
+	void enterPatternMatchingArgument(HaskellParser.PatternMatchingArgumentContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#patternMatchingArgument}.
+	 * @param ctx the parse tree
+	 */
+	void exitPatternMatchingArgument(HaskellParser.PatternMatchingArgumentContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#generalMatchingArgument}.
+	 * @param ctx the parse tree
+	 */
+	void enterGeneralMatchingArgument(HaskellParser.GeneralMatchingArgumentContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#generalMatchingArgument}.
+	 * @param ctx the parse tree
+	 */
+	void exitGeneralMatchingArgument(HaskellParser.GeneralMatchingArgumentContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#patternMatchParentheses}.
+	 * @param ctx the parse tree
+	 */
+	void enterPatternMatchParentheses(HaskellParser.PatternMatchParenthesesContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#patternMatchParentheses}.
+	 * @param ctx the parse tree
+	 */
+	void exitPatternMatchParentheses(HaskellParser.PatternMatchParenthesesContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#patternMatchingExpression}.
+	 * @param ctx the parse tree
+	 */
+	void enterPatternMatchingExpression(HaskellParser.PatternMatchingExpressionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#patternMatchingExpression}.
+	 * @param ctx the parse tree
+	 */
+	void exitPatternMatchingExpression(HaskellParser.PatternMatchingExpressionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#patternMatchingTerm}.
+	 * @param ctx the parse tree
+	 */
+	void enterPatternMatchingTerm(HaskellParser.PatternMatchingTermContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#patternMatchingTerm}.
+	 * @param ctx the parse tree
+	 */
+	void exitPatternMatchingTerm(HaskellParser.PatternMatchingTermContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#dollarSignTerm}.
+	 * @param ctx the parse tree
+	 */
+	void enterDollarSignTerm(HaskellParser.DollarSignTermContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#dollarSignTerm}.
+	 * @param ctx the parse tree
+	 */
+	void exitDollarSignTerm(HaskellParser.DollarSignTermContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#functionToMethod}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionToMethod(HaskellParser.FunctionToMethodContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#functionToMethod}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionToMethod(HaskellParser.FunctionToMethodContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#haskellFunctionToScalaMethodName}.
+	 * @param ctx the parse tree
+	 */
+	void enterHaskellFunctionToScalaMethodName(HaskellParser.HaskellFunctionToScalaMethodNameContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#haskellFunctionToScalaMethodName}.
+	 * @param ctx the parse tree
+	 */
+	void exitHaskellFunctionToScalaMethodName(HaskellParser.HaskellFunctionToScalaMethodNameContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#functionToMethodDollarSign}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionToMethodDollarSign(HaskellParser.FunctionToMethodDollarSignContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#functionToMethodDollarSign}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionToMethodDollarSign(HaskellParser.FunctionToMethodDollarSignContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#functionToMethodParen}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionToMethodParen(HaskellParser.FunctionToMethodParenContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#functionToMethodParen}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionToMethodParen(HaskellParser.FunctionToMethodParenContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#functionToMethodTerm}.
+	 * @param ctx the parse tree
+	 */
+	void enterFunctionToMethodTerm(HaskellParser.FunctionToMethodTermContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#functionToMethodTerm}.
+	 * @param ctx the parse tree
+	 */
+	void exitFunctionToMethodTerm(HaskellParser.FunctionToMethodTermContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#patternMatchArray}.
+	 * @param ctx the parse tree
+	 */
+	void enterPatternMatchArray(HaskellParser.PatternMatchArrayContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#patternMatchArray}.
+	 * @param ctx the parse tree
+	 */
+	void exitPatternMatchArray(HaskellParser.PatternMatchArrayContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#patternMatchParen}.
+	 * @param ctx the parse tree
+	 */
+	void enterPatternMatchParen(HaskellParser.PatternMatchParenContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#patternMatchParen}.
+	 * @param ctx the parse tree
+	 */
+	void exitPatternMatchParen(HaskellParser.PatternMatchParenContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#generalPatternMatchingTerm}.
+	 * @param ctx the parse tree
+	 */
+	void enterGeneralPatternMatchingTerm(HaskellParser.GeneralPatternMatchingTermContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#generalPatternMatchingTerm}.
+	 * @param ctx the parse tree
+	 */
+	void exitGeneralPatternMatchingTerm(HaskellParser.GeneralPatternMatchingTermContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#generalFunctionCall}.
+	 * @param ctx the parse tree
+	 */
+	void enterGeneralFunctionCall(HaskellParser.GeneralFunctionCallContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#generalFunctionCall}.
+	 * @param ctx the parse tree
+	 */
+	void exitGeneralFunctionCall(HaskellParser.GeneralFunctionCallContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#typeFunction}.
+	 * @param ctx the parse tree
+	 */
+	void enterTypeFunction(HaskellParser.TypeFunctionContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#typeFunction}.
+	 * @param ctx the parse tree
+	 */
+	void exitTypeFunction(HaskellParser.TypeFunctionContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link HaskellParser#haskellFunctionName}.
+	 * @param ctx the parse tree
+	 */
+	void enterHaskellFunctionName(HaskellParser.HaskellFunctionNameContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link HaskellParser#haskellFunctionName}.
+	 * @param ctx the parse tree
+	 */
+	void exitHaskellFunctionName(HaskellParser.HaskellFunctionNameContext ctx);
+}

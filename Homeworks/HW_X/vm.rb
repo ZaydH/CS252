fbaa39b0d12f0e300206c7a96c52c8145b4100ff
ZaydH <zayd.hammoudeh@sjsu.edu>
2016-05-05 01:21:54 -0700

    Store and load working in the VM.

--- Homeworks/HW_X/vm.rb
+++ Homeworks/HW_X/vm.rb
@@ -6,6 +6,9 @@ ADD_OP = /ADD/ # pops the top two elements off the stack, adds them, and puts th
 SUB_OP = /SUB/ # pops the top two elements off the stack, subtracts them, and ...
 MUL_OP = /MUL/ # pops the top two elements off the stack, multiplies them, and ...
 
+STORE_OP = /STOR (\d+)/ # Get a variable value
+LOAD_OP = /LOAD (\d+)/ # Get a variable value
+
 TRUE_SYMBOL = '#t'
 TRUE_CONSOLE_OUTPUT = 'true'
 FALSE_SYMBOL = '#f'
@@ -63,6 +66,15 @@ class VirtualMachine
                 print_val = v
             end
             puts print_val
+          # Handle the push variable
+          when STORE_OP
+            key = ln.sub(STORE_OP, '\1').chomp()
+            val = @stack.pop
+            @var_heap[key] = val
+          # Handle the push variable
+          when LOAD_OP
+            key = ln.sub(LOAD_OP, '\1').chomp()
+            @stack.push(@var_heap[key])
         else
           raise "Unrecognized command: '#{ln}'"
         end
